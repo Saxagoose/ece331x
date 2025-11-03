f3e75979742d37d55bea0ca7a6bc6a0736ea624e
Phil W. <83409445+Saxagoose@users.noreply.github.com>
2025-11-03 15:23:04 -0500

    Done!

--- module1/mod1.py
+++ module1/mod1.py
@@ -16,26 +16,31 @@ import time
 
 #variables
 fc = 433900000
-fs = 550e3
-bufferSize = 4096*2
+fs = 521e3
+bufferSize = 2**16 #play with this
 runTime = 30 #in seconds 
+bandwidth = 26e4
 samples = runTime*fs/bufferSize #number of samples
 sdr = adi.Pluto("ip:192.168.2.1")
 #configure properties
 sdr.rx_lo = int(fc) #sets Fc
 sdr.sample_rate = int(fs) #Filtercut off
-sdr.rx_rf_bandwidth = int(6e4)
-sdr.rx_buffer_size = int(1024)
-
+sdr.rx_rf_bandwidth = int(bandwidth) #play with this
+sdr.rx_buffer_size = int(bufferSize)
 data = sdr.rx()
-dataSet = np.array([])
+dataSet = np.zeros(int(samples*bufferSize), dtype=np.complex64)
 startTime = time.time()
 for i in range(int(samples)):
-    dataSet = np.concatenate((dataSet, sdr.rx()), axis=0)
-    print(i)
-    time.sleep(1/(fs)*bufferSize)
-    
+    dataSet[i*bufferSize:(i+1)*bufferSize] = sdr.rx()
+    # time.sleep(1/(fs))
 
-print("done")
-plt.specgram(dataSet, Fs=fs, NFFT=bufferSize)
+# while ((startTime-time.time())<runTime):
+#     dataSet = np.concatenate((dataSet, sdr.rx()), axis=0)
+#     time.sleep(1/(fs)*bufferSize)
+# np.save("/home/goose/Documents/wpi/ece-331x/ece331x/module1/data")
+endTime = time.time()
+print(f"done in {endTime-startTime}s")
+plt.specgram(dataSet, Fs=fs)
+plt.xlabel("Time(s)")
+plt.ylabel("Frequency(Hz)")
 plt.show()
